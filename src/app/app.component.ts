6f9d4b116b704823162b9920a0e82123b9190dc7
vonBrax <alexandrevbf84@gmail.com>
2018-05-21 18:59:16 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+export const data = [
+  { question: 'Do you agree to the terms?', answers: ['Yes', 'No'], shouldBeVisible: true  },
+  { question: 'How many grafts?', answers: ['1', '2', '3', '4'], shouldBeVisible: false  },
+  { question: 'Where do you wanna go?', answers: ['New York', 'Japan', 'Capetown'], shouldBeVisible: false  },
+  { question: 'When do you wanna go?', answers: ['Back to the future', 'Tomorrow', 'In a little while'], shouldBeVisible: false  },
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  questions = data;
+
+  changeVisibility(index: number): void {
+  this.questions[index].shouldBeVisible = !this.questions[index].shouldBeVisible;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ContainerComponent } from './container/container.component';
+import { StepComponent } from './step/step.component';
 
+import { QunoStepperModule } from './quno-stepper';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ContainerComponent,
+    StepComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    QunoStepperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
